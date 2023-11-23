7d224d071d089e02bbfd990c5eaf3dd50b6dce0c
Vivek N <vivekn262@gmail.com>
2023-11-23 16:30:54 +0530

    get circle of the radius

--- APM-Demo5/src/app/home/shell.component.ts
+++ APM-Demo5/src/app/home/shell.component.ts
@@ -8,15 +8,18 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
   numberList = [1, 6, 3, 2, 9, 10];
+  area: string;
 
   constructor() { }
 
   ngOnInit() {
     this.getMaximum();
     this.getArea();
+    console.log('testing some random chnges');
   }
 
   getMaximum() {
+    // get maximum value from given array
     return Math.min(...this.numberList);
   }
 
